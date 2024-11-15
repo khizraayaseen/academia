9574134e6f345c2512638913aa0f5f53fbe605ef
Khizra Yaseen <khizrayaseen05@gmail.com>
2024-11-15 13:39:45 -0800

    Array practice

---
+++ DSA/Arrays/practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size=1000;
+    int array[size];
+    for(int i=0; i<size; i++){
+        array[i]=1;
+        cout<<array[i];
+    }
+
+    return 0;
+}
